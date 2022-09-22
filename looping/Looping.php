3a4751e6d0968aecd4231035082ff37afca350ba
albertfndo <74182490+albertfndo@users.noreply.github.com>
2022-09-22 14:50:57 +0700

    Update Looping.php

--- looping/Looping.php
+++ looping/Looping.php
@@ -2,8 +2,7 @@
 for ($x = 1; $x <= 100; $x++) {
     if($x % 15 == 0){
         echo "TigaLima <br>";
-    }
-    else if($x % 5 == 0){
+    }else if($x % 5 == 0){
         echo "Lima <br>";
     }else if($x % 3 ==0){
         echo "Tiga <br>";
