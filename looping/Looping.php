d66060ea06510ed6d6269348bf865fd3a77ab83e
albertfndo <74182490+albertfndo@users.noreply.github.com>
2022-09-22 14:50:16 +0700

    Add files via upload

---
+++ looping/Looping.php
@@ -0,0 +1,14 @@
+<?php
+for ($x = 1; $x <= 100; $x++) {
+    if($x % 15 == 0){
+        echo "TigaLima <br>";
+    }
+    else if($x % 5 == 0){
+        echo "Lima <br>";
+    }else if($x % 3 ==0){
+        echo "Tiga <br>";
+    }else{
+        echo "$x <br>";
+    }
+}
+?>
